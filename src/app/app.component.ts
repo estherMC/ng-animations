72c5a20c7f0286cca0daf3cd81d3d860a81e087a
Esther Martorell <esther_3@hotmail.com>
2023-08-10 08:55:33 +0200

    :tada: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ng-animations2';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item: string) {
+      this.list.push(item);
+    }
+
+    onDelete(item: string){
+      //var idx = this.list.filter((product)=> item === product).indexOf
+      //this.list.slice(idx, 1);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
